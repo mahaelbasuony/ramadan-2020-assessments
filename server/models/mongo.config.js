2bc9436ca9c114e2311a44844dd3acbf306b5b0e
Maha Elbasuony <mahaelbasuony@gmail.com>
2021-10-26 01:14:08 +0200

    fetch requested videos and bind in dom

--- server/models/mongo.config.js
+++ server/models/mongo.config.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
 
-const mongodbUrl = ''; // TODO: PUT YOUR VALID MONGODB CONNECTION URL HERE <-
+const mongodbUrl = 'mongodb://localhost:27017/SampleDB'; // TODO: PUT YOUR VALID MONGODB CONNECTION URL HERE <-
 
 if (!mongodbUrl) {
-  console.log('\x1b[33m%s\x1b[0m','Please set the mongodb connection first in -> "server/models/mongo.config.js"\n');
-  return;
+    console.log('\x1b[33m%s\x1b[0m', 'Please set the mongodb connection first in -> "server/models/mongo.config.js"\n');
+    return;
 }
 
 mongoose.connect(mongodbUrl);
@@ -13,7 +13,7 @@ const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
-  console.log('Connected to Database Video Requests');
+    console.log('Connected to Database Video Requests');
 });
 
 module.exports = mongoose;
