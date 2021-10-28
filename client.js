173fab40aae80404e2b34643138841bf0e88ff24
Maha Elbasuony <mahaelbasuony@gmail.com>
2021-10-28 22:42:53 +0200

    form validation

--- client.js
+++ client.js
@@ -99,6 +99,40 @@ function debounce(fn, time) {
     timeout = setTimeout(() => fn.apply(this, args), time);
   };
 }
+
+function checkValidate(formData) {
+  const name = formData.get("author_name");
+  const email = formData.get("author_email");
+  const topic = formData.get("topic_title");
+  const topicDetails = formData.get("topic_details");
+  if (!name) {
+    document.querySelector("[name=author_name]").classList.add("is-invalid");
+  }
+  const emailPattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+  if (!email || !emailPattern.test(email)) {
+    document.querySelector("[name=author_email]").classList.add("is-invalid");
+  }
+  if (!topicDetails) {
+    document.querySelector("[name=topic_details").classList.add("is-invalid");
+  }
+  if (!topic || topic.length > 30) {
+    document.querySelector("[name=topic_title]").classList.add("is-invalid");
+  }
+  const allInvalidElms = document
+    .getElementById("formVideoRequest")
+    .querySelectorAll(".is-invalid");
+  if (allInvalidElms.length) {
+    allInvalidElms.forEach((elm) => {
+      elm.addEventListener("input", function () {
+        this.classList.remove("is-invalid");
+      });
+    });
+    return false;
+  }
+
+  return true;
+}
 document.addEventListener("DOMContentLoaded", function () {
   const formVidReqElm = document.getElementById("formVideoRequest");
 
@@ -131,11 +165,13 @@ document.addEventListener("DOMContentLoaded", function () {
   );
   formVidReqElm.addEventListener("submit", (e) => {
     e.preventDefault();
-    const fromData = new FormData(formVidReqElm);
+    const formData = new FormData(formVidReqElm);
+    const isvalid = checkValidate(formData);
+    if (!isvalid) return;
     // console.log(e);
     fetch("http://localhost:7777/video-request", {
       method: "POST",
-      body: fromData,
+      body: formData,
     })
       .then((bolb) => bolb.json())
       .then((data) => {
